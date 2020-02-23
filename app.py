8f5406cf19e016fc3f8fedefd9b200903e5d2909
Jesper Birk <jesperbirkp@gmail.com>
2020-02-23 22:04:38 +0100

    update to use postgresql on heroku

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #app.py
+import os
+
 from flask import Flask, render_template
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
@@ -6,9 +8,9 @@ from resources.honsehus import HonseHus, HH
 from models.honsehus import HonseHusModel
 
 app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///test.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
 
 
 ''' moved to run.py
